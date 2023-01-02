091c24c0e1b23b241036866ed49856db522440b6
Shelton786 <64291638+Shelton786@users.noreply.github.com>
2023-01-02 21:48:05 +0800

    机翻游戏本体
    2023年1月2日
    汉化人员：
    平凡的书儿（https://space.bilibili.com/113589678）
    机翻全部内容
    但是被工具快整疯了
    求求来个会js反向或会科学上网的老哥吧！

---
+++ HHW1.0.9_ZH/与工具一同启动.bat
@@ -0,0 +1,4 @@
+chcp 65001
+cd /d "%~dp0"
+"E:\galgame\MTool\Tool\loaders\inject.exe" "%~dp0\Game.exe" "E:\galgame\MTool\Tool\loaders\mvHook.dll"
+start "" "E:\galgame\MTool\Tool\nw.exe" "E:\galgame\MTool\Tool"
