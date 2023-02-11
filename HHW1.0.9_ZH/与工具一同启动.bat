9499621937c9ae5a4c147d414d90135250285c88
Shelton786 <64291638+Shelton786@users.noreply.github.com>
2023-02-11 14:26:20 +0800

    Delete HHW1.0.9_ZH directory

--- HHW1.0.9_ZH/与工具一同启动.bat
+++
@@ -1,4 +0,0 @@
-chcp 65001
-cd /d "%~dp0"
-"E:\galgame\MTool\Tool\loaders\inject.exe" "%~dp0\Game.exe" "E:\galgame\MTool\Tool\loaders\mvHook.dll"
-start "" "E:\galgame\MTool\Tool\nw.exe" "E:\galgame\MTool\Tool"
